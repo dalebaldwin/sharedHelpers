2154eb971cdf78ca1573717c79c327a2c16f0a98
Dale Baldwin <dale@atypical.net.au>
2020-06-18 16:19:36 +1000

    Work on theming components from shared

---
+++ dist/theme/themeFunctions.d.ts
@@ -0,0 +1,2 @@
+declare const ButtonStyle: (theme: any) => import("styled-components").FlattenSimpleInterpolation;
+export default ButtonStyle;
---
+++ src/theme/themeFunctions.ts
@@ -0,0 +1,7 @@
+import {css} from 'styled-components'
+
+const ButtonStyle = (theme) => css`
+  background-color: ${theme.buttonColor}
+`
+
+export default ButtonStyle
